efeabcf8abde61b5011aa0978e5514e62d590c0f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-29 20:04:50 -0300

    feat: adicionando trycatch

--- src/modules/urlShrink/database/typeorm/repositories/urlRepository.ts
+++ src/modules/urlShrink/database/typeorm/repositories/urlRepository.ts
@@ -7,15 +7,20 @@ export default class UrlRepository implements IUrlRepository {
   private ormRepository = getRepository(Url);
 
   async create(url: string): Promise<Url> {
-    const urlExists = await this.ormRepository.findOne({ where: { originalUrl: url }});
-    
-    if (urlExists) return urlExists;
-
-    const shortUrl = crypto.randomBytes(3).toString('hex');
-    const urlToSave = this.ormRepository.create({ shortUrl, originalUrl: url });
-    await this.ormRepository.save(urlToSave);
-    
-    return urlToSave;
+    try {
+      const urlExists = await this.ormRepository.findOne({ where: { originalUrl: url }});
+      
+      if (urlExists) return urlExists;
+  
+      const shortUrl = crypto.randomBytes(3).toString('hex');
+      const urlToSave = this.ormRepository.create({ shortUrl, originalUrl: url });
+      await this.ormRepository.save(urlToSave);
+      
+      return urlToSave;
+    } catch (error) {
+      const newData = await this.create(url)
+      return newData;
+    }
   }
   async find(shortUrl: string): Promise<Url> {
     const originalUrl = await this.ormRepository.findOne({ where: { shortUrl }});
