21b980b962ae65416bcc0a17f801609697d5bae9
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-29 10:42:46 -0300

    chore: adicionando dependencias

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -6,6 +6,6 @@ app.use('/', (_req: Request, res: Response) =>
   res.status(302).redirect('http://google.com.br')
 );
 
-app.listen(3000, 'localhost', () => {
-  console.log(`Starting Proxy at ${'localhost'}:${3000}`);
+app.listen(3000, () => {
+  console.log(`Starting Proxy at 3000`);
 });
