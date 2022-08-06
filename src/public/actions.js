d27feb8b4f0f218287a778477bd7ea51a391848c
riccihenrique <ricci.henrique@outlook.com>
2022-08-05 21:21:33 -0300

    add reload

--- src/public/actions.js
+++ src/public/actions.js
@@ -1,6 +1,16 @@
 const button = document.getElementById('submitButton');
 const openUrlButton = document.getElementById('openUrl');
 
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
+
 const baseURL = 'https://short.wtf';
 
 button.addEventListener('click', async () => { 
