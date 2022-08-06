6096ad1ee54f1e656bbf0c41685f39bc6d99b7be
riccihenrique <ricci.henrique@outlook.com>
2022-08-05 21:10:23 -0300

    fix html

--- src/public/actions.js
+++ src/public/actions.js
@@ -1,16 +1,31 @@
-const button = document.getElementById('submitButton')
+const button = document.getElementById('submitButton');
+const openUrlButton = document.getElementById('openUrl');
+
+const baseURL = 'https://short.wtf';
 
 button.addEventListener('click', async () => { 
-  const input = document.getElementById('url')
-  const shortUrl = await axios.post('https://short.wtf/', {
+  const input = document.getElementById('shortUrl')
+  const shortUrl = await axios.post(baseURL, {
     url: input.value
   })
-  const shortUrlDiv = document.getElementById('shortUrl')
-  const newUrl = document.createElement('a')
-  newUrl.href = shortUrl.data.newUrl
-  newUrl.className = 'generated-url'
-  newUrl.innerText = shortUrl.data.newUrl;
-  shortUrlDiv.appendChild(newUrl)
-})
 
-axios.get('https://short.wtf/cont')
+  const { data: { newUrl } } = shortUrl;
+
+  input.value = newUrl;
+  /* Select the text field */
+  input.select();
+  input.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(input.value);
+  button.type = 'hidden'
+  document.getElementById('openUrl').type = 'button';
+
+  document.getElementById('urlmessage').value = `URL ${newUrl} copiada com sucesso`;  
+});
+
+openUrlButton.addEventListener('click', () => {
+  window.location.href = document.getElementById('shortUrl').value;
+});
+
+axios.get(`${baseURL}/cont`);
